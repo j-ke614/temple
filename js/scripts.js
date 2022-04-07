5c9d12a0ed6f05963164429f05765be95035cd3b
j-ke614 <corn614@gmail.com>
2022-04-06 23:37:26 -0600

    commit 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,3 @@
-$.getJSON("https://api.openweathermap.org/data/2.5/forecast?lat=42.8713&lon=-112.4455&appid={1fc4486682b178960a91c0c0d6b95b25}"), 
-
-function(data){
-
-console.log(data);
-
-var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-
-var temp = Math.floor(data.main.temp);
-
-var weather = data.weather[0].main;
-
-
-$('.icon').attr('src', icon);
-$(".temp").append(temp  + ' &#8457;');
-
-$(".weather").append(weather );
-};
-
 function toggleMenu(){
     document.getElementById('primaryNav').classList.toggle('open');   
     document.getElementById('hamburgerBtn').classList.toggle('open');   
@@ -30,3 +11,14 @@ const latestUp = document.querySelector('#latestUp');
 const update = document.lastModified
 
 latestUp.innerHTML = update
+
+//date
+const currentDate = document.querySelector('#currentDate');
+const currentYear = document.querySelector('#currentYear');
+
+const now = new Date()
+const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
+const Year = now.getFullYear()
+
+currentDate.innerHTML = fulldate
+currentYear.innerHTML = Year
